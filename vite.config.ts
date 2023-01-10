5f117f946afd9520c9e8f5f411ade5024625fb03
medreres <90519417+medreres@users.noreply.github.com>
2023-01-10 19:38:05 +0200

    icon for tab

--- vite.config.ts
+++ vite.config.ts
@@ -23,14 +23,28 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      manifest: {
+        name: "Weather App",
+        short_name: "Weather",
+        description: "Weather prediction for the following week",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icon.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+        ],
+      },
       workbox: {
         runtimeCaching: [
           getCache({
-            pattern: 'https://api.open-meteo.com/v1/forecast?latitude=49.84&longitude=24.02&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=Europe%2FMoscow',
+            pattern:
+              "https://api.open-meteo.com/v1/forecast?latitude=49.84&longitude=24.02&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=Europe%2FMoscow",
             name: "forecast",
           }),
           getCache({
-            pattern: 'https://www.gstatic.com/charts/loader.js',
+            pattern: "https://www.gstatic.com/charts/loader.js",
             name: "charts",
           }),
           // getCache({
