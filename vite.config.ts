99348d9be55210a37b8ba08d623a9d61f3abf230
medreres <90519417+medreres@users.noreply.github.com>
2023-01-19 19:31:19 +0200

    fix regex for caching weather requests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import path from 'path'
+import path from "path";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
@@ -37,18 +37,24 @@ export default defineConfig({
         theme_color: "#212529",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "favicon-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "favicon-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
+            src: "favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "favicon-16x16.png",
+            sizes: "16x16",
             type: "image/png",
             purpose: "any maskable",
           },
@@ -57,15 +63,11 @@ export default defineConfig({
       workbox: {
         runtimeCaching: [
           getCache({
-            pattern:
-              /https:\/\/api\.open-meteo\.com\/v1\/forecast\?latitude=(.*)&longitude=(.*)&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=(.*)/gm,
+            pattern: /.*api.open-meteo.*/gm,
             name: "forecast",
           }),
         ],
       },
-      // devOptions: {
-      //   enabled: true,
-      // },
     }),
   ],
   test: {
